f8e326859ca6131b733d7ef87923f81f8caeeed1
Shubh <shubh2268@gmail.com>
2025-02-11 19:53:50 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+
+  return (
+    <div className='text-5xl font-bold underline text-blue-400'>
+     Hello Shubham!
+    </div>
+  )
+}
+
+export default App;
